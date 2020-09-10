8161ed68c7b9a6cda0b09b60ef06253cc984573e
unknown <gomathigunasekaran1998@gmail.com>
2020-09-10 14:09:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,468 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//        <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+// import React, { Component } from "react";
+ 
+// class Main extends Component {
+//   render() {
+//     return (
+//         <div>
+//           <h1>Simple SPA</h1>
+//           <ul className="header">
+//             <li><a href="/">Home</a></li>
+//             <li><a href="/stuff">Stuff</a></li>
+//             <li><a href="/contact">Contact</a></li>
+//           </ul>
+//           <div className="content">
+             
+//           </div>
+//         </div>
+//     );  
+//   }
+// }
+ 
+// export default Main;
+
+
+
+
+import React, { Component } from "react";
+import bizamp_ceo from './bizamp_ceo.jpg';
+import facebook_logo1_pro from'./facebook_logo1_pro.png';
+import instagram_logo_pro from'./instagram_logo_pro.png';
+import linked_in_pro from './linked_in_pro.png';
+import slidee_logo_pro from './slidee_logo_pro.png';
+import twitter_logo_pro from './twitter_logo_pro.png';
+import utube_icon_pro from './utube_icon_pro.jpg';
+//import {Container} from '@material-ui/core';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flx">
+
+    
+    
+    <div className="App">
+     <div className="bizamps">
+       <h1>BIZAMPS</h1>
+     </div>
+
+     <div className="Expertise">
+       <h1>Expertise</h1>
+     </div>
+
+     <div className="Pricing">
+       <h1>Pricing</h1>
+     </div>
+
+     <div className="Case_Stories">
+       <h1>Case Stories</h1>
+     </div>
+      
+    </div>
+    
+    <div className="second-part1">
+
+    </div>
+
+    <div className="imagee">
+          <img src={bizamp_ceo} width="400" height="400"   />
+        </div>
+
+    <div className="second-part2">
+    </div>
+
+    <div className="line1">
+    <h1>"I WENT THROUGH EVERY</h1>
+    
+    </div>
+
+    <div className="line2">
+    <h1>ACCOUNT-BASED MARKETING</h1>
+    
+    </div>
+
+    <div className="line3">
+    <h1>WEBINAR, TALK & EBOOK AND</h1>
+    
+    </div>
+
+    <div className="line4">
+    <h1>HERE'S THE BEST OF THEM!"</h1>
+    
+    </div>
+
+    <div className="line5">
+    <h1>Note : ABM is strategy, below mentioned are </h1>
+    
+    </div>
+
+    <div className="line6">
+    <h1>tactics & techniques</h1>
+    
+    </div>
+
+    <div className="table_of_contents">
+    
+    </div>
+
+    <div className="content">
+    <h1>Table of Contents:</h1>
+    </div>
+
+    <div className="bx1">
+     </div>
+
+     <div className="bx1_1">
+     <h1>1</h1>
+     </div>
+
+     <div className="bx1_con">
+     <h1>Benchmarks & Results of Account-Based Marketing</h1>
+     </div>
+
+     <div className="bx2">
+     </div>
+
+     <div className="bx2_2">
+     <h1>2</h1>
+     </div>
+
+     <div className="bx2_con">
+     <h1>ABM examples vs B2B Demand Generation</h1>
+     </div>
+
+     <div className="bx3">
+     </div>
+
+     <div className="bx3_3">
+     <h1>3</h1>
+     </div>
+
+     <div className="bx3_con">
+     <h1>How to use old content & better messaging (Video: skip to 13.37 mins mark)</h1>
+     </div>
+
+     <div className="bx4">
+     </div>
+
+     <div className="bx4_4">
+     <h1>4</h1>
+     </div>
+
+     <div className="bx4_con">
+     <h1>Why sales in ABM?</h1>
+     </div>
+
+     <div className="bx5">
+     </div>
+
+     <div className="bx5_5">
+     <h1>5</h1>
+     </div>
+
+     <div className="bx5_con">
+     <h1>Sales responsibility in ABM</h1>
+     </div>
+
+     <div className="bx6">
+     </div>
+
+     <div className="bx6_6">
+     <h1>6</h1>
+     </div>
+
+     <div className="bx6_con">
+     <h1>Backend & Tech Stack for ABM(For scaling)</h1>
+     </div>
+
+     <div className="bx7">
+     </div>
+
+     <div className="bx7_7">
+     <h1>7</h1>
+     </div>
+
+     <div className="bx7_con">
+     <h1>Points to Note before starting</h1>
+     </div>
+
+     <div className="bx8">
+     </div>
+
+     <div className="bx8_8">
+     <h1>8</h1>
+     </div>
+
+     <div className="bx8_con">
+     <h1>How to Select Accounts(The 5 Levels)</h1>
+     </div>
+
+     <div className="bx9">
+     </div>
+
+     <div className="bx9_9">
+     <h1>9</h1>
+     </div>
+
+     <div className="bx9_con">
+     <h1>BizAmp's Step by Step ABM Pilot</h1>
+     </div>
+
+     <div className="content1">
+     </div>
+ 
+     <div className="content1_bx">
+     
+     </div>
+
+     <div className="content1_in">
+     <h1>1</h1>
+     </div>
+
+     <div className="content1_inside">
+     <h1>Benchmarks & Results of Account-Based Marketing</h1>
+     </div>
+
+     <div className="content2">
+     </div>
+ 
+     <div className="content2_bx">
+     
+     </div>
+
+     <div className="content2_in">
+     <h1>2</h1>
+     </div>
+
+     <div className="content2_inside">
+     <h1>ABM examples vs B2B Demand Generation</h1>
+     </div>
+
+
+     <div className="content3">
+     </div>
+ 
+     <div className="content3_bx">
+     
+     </div>
+
+     <div className="content3_in">
+     <h1>3</h1>
+     </div>
+
+     <div className="content3_inside">
+     <h1>How to use old content & better messaging (Video: skip to 13.37 mins mark)</h1>
+     </div>
+
+
+     <div className="content4">
+     </div>
+ 
+     <div className="content4_bx">
+     
+     </div>
+
+     <div className="content4_in">
+     <h1>4</h1>
+     </div>
+
+     <div className="content4_inside">
+     <h1>Why sales in ABM?</h1>
+     </div>
+
+     <div className="content5">
+     </div>
+ 
+     <div className="content5_bx">
+     
+     </div>
+
+     <div className="content5_in">
+     <h1>5</h1>
+     </div>
+
+     <div className="content5_inside">
+     <h1>Sales responsibility in ABM</h1>
+     </div>
+
+     <div className="content6">
+     </div>
+ 
+     <div className="content6_bx">
+     
+     </div>
+
+     <div className="content6_in">
+     <h1>6</h1>
+     </div>
+
+     <div className="content6_inside">
+     <h1>Backend & Tech Stack for ABM(For scaling)</h1>
+     </div>
+
+     <div className="content7">
+     </div>
+ 
+     <div className="content7_bx">
+     
+     </div>
+
+     <div className="content7_in">
+     <h1>7</h1>
+     </div>
+
+     <div className="content7_inside">
+     <h1>Points to Note before starting</h1>
+     </div>
+
+     <div className="content8">
+     </div>
+ 
+     <div className="content8_bx">
+     
+     </div>
+
+     <div className="content8_in">
+     <h1>8</h1>
+     </div>
+
+     <div className="content8_inside">
+     <h1>How to Select Accounts(The 5 Levels)</h1>
+     </div>
+
+     <div className="content9">
+     </div>
+ 
+     <div className="content9_bx">
+     
+     </div>
+
+     <div className="content9_in">
+     <h1>9</h1>
+     </div>
+
+     <div className="content9_inside">
+     <h1>BizAmp's Step by Step ABM Pilot</h1>
+     </div>
+
+
+    <div className="home">
+    <h1>Home  </h1>
+    
+    </div>
+    
+    <div className="sym1">
+    <h1>{">>"}</h1>
+    </div>
+
+    <div className="pricing">
+    <h1>Pricing</h1>
+    
+    </div>
+
+
+    <div className="sym2">
+    <h1>{">>"}</h1>
+    </div>
+
+    <div className="marketing">
+    <h1>B2B Marketing Pakages</h1>
+    
+    </div>
+
+
+    <div class="bottom">
+
+    </div>
+
+    <div className="icon1">
+    <img src={utube_icon_pro} width="60" height="50" alt=""  />
+  </div>
+
+  <div className="icon2">
+    <img src={slidee_logo_pro} width="60" height="50" alt="" />
+  </div>
+
+  <div className="icon3">
+    <img src={linked_in_pro} width="60" height="50" alt=""  />
+  </div>
+
+  <div className="icon4">
+    <img src={instagram_logo_pro} width="60" height="50" alt="" />
+  </div>
+
+  <div className="icon5">
+    <img src={facebook_logo1_pro} width="60" height="50" alt="" />
+  </div>
+
+  <div className="icon6">
+    <img src={twitter_logo_pro} width="60" height="50" alt=""  />
+  </div>
+
+  <div className="linee">
+  
+  </div>
+
+  <div className="bx_line1">
+  <h1>BizAmps is B2B marketing consultancy based out of India.The team is spread across</h1>
+  </div>
+
+  <div className="bx_line2">
+  <h1>Delhi NCR,Chandigarh & Mumbai</h1>
+  </div>
+
+  <div className="bx_line3">
+  <h1>All Rights Reserved © Copyright 2020</h1>
+ 
+  </div>
+
+  <div className="first_box"></div>
+
+  <div className="left_box"></div>
+
+  <div className="left_box1"></div>
+
+  <div className="right_box"></div>
+  
+
+  <div className="bottom_box"></div>
+
+
+</div>
+
+    
+  );
+}
+
+export default App;
---
+++ src/imagee.js
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import bizamp_ceo from './bizamp_ceo.jpg'; // Tell webpack this JS file uses this image
+// //console.log(logo); // /logo.84287d09.png
+// function Header() {
+//   // Import result is the URL of your image
+//   return <img src={bizamp_ceo} alt="bizamp_ceo" />;
+// }
+// export default Header;
